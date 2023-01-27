5916ef0a276a582c63a9dcb9797acf5c365d6001
Thomas BANCE-LAUPENIE <thomas170303@gmail.com>
2023-01-27 09:58:09 +0100

    transport livreur

--- transport.js
+++ transport.js
@@ -1,12 +1,34 @@
-const primePlafond = 900, primeDistance = 0, premierAncien = 300;
-let nbAncien = parseInt(window.document.querySelector("#num_ancien").value);
+const distance = 0.01, distance_max = 900, ancien_fixe = 300, ancien_up = 30;
+let primeA = 0, primeD = 0; //PrimeA = Prime d'ancienneté; Prime D = Prime de distance;
 
-function primeAncien(nbAncien, premierAncien) {
-    if (nbAncien >= 4) {
-        nbAncien -= 4;
-        return nbAncien * 30 + premierAncien;
+
+/*
+ Fonction qui renvoie la prime d'ancienneté
+ ancien, integer qui correspond aux nombres d'années dans l'entreprise.
+ */
+function prime_ancien(ancien) {
+    if (ancien < 4) { //condition vrai si le temps passer dans l'entreprise est inférieur à ans.
+        return(primeA = 0); //Renvoie le total de la prime d'ancienneté.
+    } else { //égale ou supérieur à 4 ans
+        primeA = ancien_fixe;
+
+        if (ancien > 4) { //Supérieur à 4 ans
+            primeA = ancien_fixe + ((ancien - 4) * ancien_up);
+        }
     }
-    else{
-        return 0;
-    }       
+    return primeA; //renvoie la valeur total de la prime d'ancienneté.
 }
+
+/*
+ Fonction qui renvoi la prime de distance
+ km, integer qui correspond aux nombre de kilomettres parcourus au courant de cette année
+ */
+function prime_distance(km) {
+    primeD = distance * km; // Ajoute à la prime, la valeur par kilomettre parcouru
+    if (primeD > 900) { //si la prime est supérieur au maximum autoriser par l'entreprise, renvoie la valeur de la prime au maximum défini.
+        primeD = 900;
+    }
+    return primeD; //renvoi la valeur total de la prime de distance.
+}
+
+let prime = primeA + primeD; // La prime est égal à la prime de distance + la prime d'ancienneté.
