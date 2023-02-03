552896fa008a5d20fc1ab4c00440ffb17ad01f5d
Thomas BANCE-LAUPENIE <thomas170303@gmail.com>
2023-02-03 11:41:33 +0100

    fin correction exercice

--- js/alcoolemie.js
+++ js/alcoolemie.js
@@ -9,6 +9,7 @@ function getAlcoolPur(nbVerres) {
     const uniteAlcool = 10;
     return uniteAlcool * nbVerres;
 }
+
 /**
  * Fonction qui retourne le coefficient de diffusion en fonction du sexe
  *
@@ -23,3 +24,52 @@ function getCoefDiffusion(sexe) {
         return coefDiffuF;
     }
 }
+
+/**
+ * Fonction qui retourne l'alcoolémie en fonction du sexe, du poids et du
+ * nombre de verres ingérés
+ *
+ * @param {string} sexe
+ * @param {int} poids
+ * @param {int} nbVerres
+ * @returns {float}
+ */
+function getAlcoolemie(sexe, poids, nbVerres) {
+// /!\ division par 0, on ne veut pas provoquer la destruction de l'univers ;o)
+    if (poids > 0) {
+        return (getAlcoolPur(nbVerres) / (poids * getCoefDiffusion(sexe))).toFixed(2);
+    } else {
+        return 0;
+    }
+}
+
+/**
+ * Fonction qui retourne l'amende encourue en fonction de l'alcoolémie
+ *
+ * @param {float} alcoolemie
+ * @returns {string}
+ */
+function getAmende(alcoolemie) {
+    const seuil = 0.8;
+    if (alcoolemie < seuil) {
+        return 'Minorée : 90 € / Forfaitaire : 135 € / Majorée : 375 €';
+    } else {
+        return '4500 €';
+    }
+}
+
+/**
+ * Fonction qui retourne la sanction encourue en fonction de l'alcoolémie
+ *
+ * @param {float} alcoolemie
+ * @returns {string}
+ */
+function getSanction(alcoolemie) {
+    const seuil = 0.8;
+    if (alcoolemie < seuil) {
+        return '6 points + suspension 3 ans';
+    } else {
+        return '6 points + 2 ans de prison + suspension 3 ans + stage de sensibilisation';
+    }
+}
+
