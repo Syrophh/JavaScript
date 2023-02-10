b8959df2ad1d913674565a296b803b8884ff60aa
Thomas BANCE-LAUPENIE <thomas170303@gmail.com>
2023-02-10 11:50:49 +0100

    interface alcoolémie v2

--- js/alcoolemie.js
+++ js/alcoolemie.js
@@ -99,3 +99,23 @@ function getInt(id) {
 function getString(id) {
     return window.document.querySelector(id).value;
 }
+
+window.addEventListener('load', function () {
+    // tabEvents est une collection d'évenements
+    let tabEvents = ['keyup', 'click'];
+    // tabInputs est une collection de <input>
+    let tabInputs = window.document.querySelectorAll('input[type="number, radio"]');
+    // Parcours de tabInputs en s'appuyant sur le nombre de <input> et sur tabEvents
+    for (let i = 0; i < tabInputs.length; i++) {
+        for (let j = 0; j < tabEvents.length; j++) {
+            // Ajout des listeners sur tous les <input> des events listés dans tabEvents
+            tabInputs[i].addEventListener(tabEvents[j], getAlcoolemie);
+        }
+    }
+    // Gestion de l'input de type range (recopie de la valeur dans l'output)
+    window.document.querySelector('#nb_accidents').addEventListener('change', function () {
+        window.document.querySelector('#o_nb_accidents').value =
+                recupValeur('#nb_accidents');
+        getAlcoolemie();
+    });
+});
